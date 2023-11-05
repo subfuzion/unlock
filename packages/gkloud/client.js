dbb13d1345a654d5202d037e28204bc89a5db014
Tony Pujals <tpujals@gmail.com>
2023-11-05 13:07:22 -0800

    fix: lint errors

--- packages/gkloud/client.js
+++ packages/gkloud/client.js
@@ -1,6 +1,4 @@
-module.exports = {
-  unlock
-}
+module.exports = {unlock}
 
 //
 // NOTES
@@ -10,32 +8,37 @@ module.exports = {
 //
 
 /**
- * @typedev {Object} TerminalInfo
+ * Terminal properties spec
+ * @typedef  {Object}  TerminalInfo
  * @property {boolean} [istty=true] Allow terminal format codes
- * @property {number} [width] Terminal width
- * @property {number} [height] Terminal height
+ * @property {number}  [width]      Terminal width
+ * @property {number}  [height]     Terminal height
  */
 
 /**
- * @typedev {Object} UnlockRequest
- * @property {string[]}             args     Command positional arguments
- * @property {Object.<string,any>}  options  Command options map
- * @property {TerminalInfo}         terminfo Terminal information
+ * Request spec for the unlock api
+ * @typedef  {Object}              UnlockRequest
+ * @property {string[]}            args     Command positional arguments
+ * @property {Object.<string,any>} options  Command options map
+ * @property {TerminalInfo}        terminfo Terminal information
  */
 
 /**
- * @typedef {Object} UnlockResponse
- * @property {number}  code     API HTTP response status code
- * @property {string}  content  Response content to display
+ * Response spec for the unlock api
+ * @typedef  {Object} UnlockResponse
+ * @property {number} code    API HTTP response status code
+ * @property {string} content Response content to display
  */
 
 /**
- * @param {UnlockRequest} request
+ * @param  {UnlockRequest} request
  * @return {Promise<UnlockResponse>}
  */
 async function unlock(request) {
-  return await Promise.resolve({
-    code: 200,
-    content: "Hello, world!",
+  console.log("unlock request:", request);
+  return new Promise((resolve) => {
+    resolve({
+      code: 200, content: "Hello, world!",
+    })
   });
 }
--- packages/gkloud/main.js
+++ packages/gkloud/main.js
@@ -1,54 +1,50 @@
-const { Command } = require("commander");
+const {Command} = require("commander");
 
 const {unlock} = require("./client.js");
 
-const {
-  description,
-  version,
-} = require("./package.json");
+const {description, version} = require("./package.json");
 
-module.exports = {
-  run,
-};
+module.exports = {run};
 
 async function run() {
   const cli = new Command();
   cli
-    .name("gkloud")
-    .description(description)
-    .version(version)
-    .helpOption(false)
+      .name("gkloud")
+      .description(description)
+      .version(version)
+      .helpOption(false)
 
-  // hack: delete default version option
+  // hack: delete default version option (ignore cli.options readonly)
+  // eslint-disable
+  // noinspection JSConstantReassignment
   cli.options = cli.options.filter(item => item.long !== "--version");
+  // eslint-enable
 
   // add dedicated version command
   cli.command("version")
-    .description("Print version")
-    .action(() => {
-      console.log(version);
-    });
+      .description("Print version")
+      .action(() => {
+        console.log(version);
+      });
 
   // add unlock command
   cli.command("unlock")
-    .description("Display information about Google Cloud events")
-    .allowExcessArguments(true)
-    .action(async (options, cmd) => {
-      /** @type {TerminalInfo} */
-      let terminfo = {
-	tty: true,
-      }
+      .description("Display information about Google Cloud events")
+      .allowExcessArguments(true)
+      .action(async (options, cmd) => {
+        /** @type {import("./client.js").TerminalInfo} */
+        let terminfo = {
+          tty: true,
+        }
 
-      /** @type {UnlockRequest} */
-      let request = {
-	args: cmd.args,
-	options: options,
-	terminfo: terminfo,
-      }
+        /** @type {import("./client.js").UnlockRequest} */
+        let request = {
+          args: cmd.args, options: options, terminfo: terminfo,
+        }
 
-      let response = await unlock(request);
-      console.log(response);
-    });
+        let response = await unlock(request);
+        console.log(response);
+      });
 
   await cli.parseAsync();
 }
